76681bcb6be098f0a0c63f361b84bd8c4d4bbb4f
Jasdeep Kaur <jasdeepkaur1010@gmail.com>
2023-06-14 21:11:06 -0500

    Created play file, established connection with server and got snake in the snek-multiplayer

---
+++ play.js
@@ -0,0 +1,20 @@
+const net = require("net");
+//establishes a connection with the game server
+const connect = function (data) {
+  const conn = net.createConnection({
+    host: "172.27.143.54",
+    port: 50541,
+  });
+
+  //interpret incoming data as text
+  conn.setEncoding("utf8");
+  // return conn;
+
+
+conn.on("connect", () => {
+  console.log("Connection Established");
+  return conn;
+});
+};
+console.log("Connecting...");
+connect();
